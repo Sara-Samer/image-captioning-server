7199f4c1103e3b48ec689f6313507f0afe7df9b4
Sara Samer <ss.titanic199@gmail.com>
2022-08-07 00:06:15 +0200

    fixed ui

--- upload.py
+++ upload.py
@@ -5,6 +5,9 @@ from flask_uploads import UploadSet, configure_uploads, IMAGES
 from inference import infer_caption
 from models import get_cnn_model
 import os
+from PIL import Image
+import base64
+import io
 # from keras.preprocessing.image import load_img
 # the pretrained model
 # from model import process_image, predict_class
@@ -18,32 +21,38 @@ app.config['UPLOADED_PHOTOS_DEST'] = './static/img'
 configure_uploads(app, photos)
 
 # professionals have standards :p
-@app.route('/home', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def home():
     welcome = "Hello, World !"
     return welcome
 
 # the main route for upload and prediction
-# @app.route('/caption', methods=['GET', 'POST'])
-# def upload():
-#     if request.method == 'POST' and 'photo' in request.files:
-#         # save the image
-#         filename = photos.save(request.files['photo'])
-#         # load the image
-#         img_path = './static/img/' + filename
-#         caption = infer_caption(img_path)
-#         os.remove(img_path)
-#         # image = load_img('./static/img/'+filename, target_size=(224, 224))
-#         # process the image
-#         # image = process_image(image)
-#         # make prediction
-#         # prediction, percentage = predict_class(image)
-#         # the answer which will be rendered back to the user
-#         return {'caption': caption}
-#     # web page to show before the POST request containing the image
-#     return render_template('upload.html')
-@app.route('/caption', methods=['POST'])
+@app.route('/caption', methods=['GET', 'POST'])
 def upload():
+    if request.method == 'POST' and 'photo' in request.files:
+        print('inferring')
+        # save the image
+
+        filename = photos.save(request.files['photo'])
+        # load the image
+        img_path = './static/img/' + filename
+        caption = infer_caption(img_path)
+        im = Image.open(img_path)
+        data = io.BytesIO()
+        #First save image as in-memory.
+        im.save(data, "JPEG")
+        #Then encode the saved image file.
+        encoded_img_data = base64.b64encode(data.getvalue())
+
+        os.remove(img_path)
+
+        return render_template("caption.html", img_data=encoded_img_data.decode('utf-8'), caption=caption)
+
+    # web page to show before the POST request containing the image
+    return render_template('upload.html')
+
+@app.route('/caption-api', methods=['POST'])
+def caption():
     # save the image
     filename = photos.save(request.files['photo'])
     # load the image
@@ -54,15 +63,9 @@ def upload():
     return jsonify({'caption': caption})
     # web page to show before the POST request containing the image
 
-# @app.route("/im_size", methods=["POST"])
-# def process_image():
-#     file = request.files['image']
-#     # Read the image via file.stream
-#     img = Image.open(file.stream)
-#     return jsonify({'msg': 'success', 'size': [img.width, img.height]})
-
 
 
 if __name__ == '__main__':
     get_cnn_model()
+
     app.run(host='0.0.0.0', debug=True)
